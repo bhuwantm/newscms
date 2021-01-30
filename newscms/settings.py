5ef44423c0c3a99ebf47059de715c1e6a20cb426
bhagwan <bhuwantm@gmail.com>
2021-01-30 14:45:47 +0545

    - added sample fixture

--- newscms/settings.py
+++ newscms/settings.py
@@ -110,11 +110,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'ne'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'UTC'
 
-USE_I18N = False
+USE_I18N = True
 
 USE_L10N = True
 
